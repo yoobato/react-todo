b0daf1b9c1e9a5c197cfa2b2405cf94866cbe764
Daeyeol Ryu <yoobato@gmail.com>
2022-01-29 14:46:29 +0900

    Add Delete button

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -23,7 +23,16 @@ const ToDo = ({ text, category, id }: IToDo) => {
     });
   };
 
-  // TODO: 삭제 버튼 추가
+  const onDeleteClick = () => {
+    setToDos((oldToDos) => {
+      const oldToDoIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      // Chagne oldToDo to newToDo
+      return [
+        ...oldToDos.slice(0, oldToDoIndex),
+        ...oldToDos.slice(oldToDoIndex + 1)
+      ]
+    });
+  };
 
   return (
     <li>
@@ -31,6 +40,7 @@ const ToDo = ({ text, category, id }: IToDo) => {
       {category !== Categories.DOING && <button name={Categories.DOING} onClick={onClick}>Doing</button>}
       {category !== Categories.TO_DO && <button name={Categories.TO_DO} onClick={onClick}>To Do</button>}
       {category !== Categories.DONE && <button name={Categories.DONE} onClick={onClick}>Done</button>}
+      <button onClick={onDeleteClick}>Delete</button>
     </li>
   );
 };
